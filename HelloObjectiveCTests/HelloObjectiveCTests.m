1242ee5196c24350dc1ff2b3e260a4af0e375a00
bmoser <basil.moser@gmx.ch>
2013-03-10 18:22:16 +0100

    more simple tests

--- HelloObjectiveCTests/HelloObjectiveCTests.m
+++ HelloObjectiveCTests/HelloObjectiveCTests.m
@@ -10,8 +10,6 @@
 #import "MyClass.h"
 #import "MyClass+Additions.h"
 
-// Seite 85
-
 @implementation HelloObjectiveCTests
 
 - (void)setUp
@@ -58,7 +56,77 @@
     NSNumber *someNumber = [[NSNumber alloc] initWithInt:22];
     NSNumber *numberTwo = [NSNumber numberWithBool:TRUE];
     
+    NSInteger nsInt = 6;
+    nsInt = nsInt *3;
+    NSLog(@"%li", (long)nsInt);
     
+    // Array
+    NSArray *someArray =
+    [NSArray arrayWithObjects:m, i, @"Hello World", nil];
+    
+    NSUInteger size = [someArray count];
+    
+    for (int i = 0; i < size; i++)
+    {
+        NSLog(@"Item %i is: %@", i, [someArray objectAtIndex:i]);
+    }
+    
+    NSMutableArray *mutableArray = [NSMutableArray array];
+    [mutableArray addObject:@"gamma"];
+    [mutableArray addObject:@"alpha"];
+    [mutableArray addObject:@"beta"];
+    [mutableArray replaceObjectAtIndex:0 withObject:@"epsilon"];
+    
+    [mutableArray sortUsingSelector:@selector(caseInsensitiveCompare:)];
+    
+    [self printArrayToConsole:mutableArray];
+    
+    // must end with nil!
+    NSSet *simpleSet = [NSSet setWithObjects:@"Hello, World!", @42, m, nil];
+    NSUInteger setCount = [simpleSet count];
+    NSLog(@"%lu", (unsigned long)setCount);
+    
+    NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                m, @"anObject",
+                                @"Hello, World!", @"helloString",
+                                @42, @"magicNumber",
+                                setCount, @"aValue",
+                                nil];
+    
+    // NSMutableDirectory
+    //[dictionary setObject:@"another string" forKey:@"secondString"];
+    //[dictionary removeObjectForKey:@"anObject"];
+    
+    // File write
+    //NSURL *fileURL =
+    //NSArray *arrayFile = @[@"first", @"second", @"third"];
+    //BOOL success = [arrayFile writeToURL:fileURL atomically:YES];
+    //if (!success) {
+        // an error occured...
+    //}
+    
+    // Blocks aka. callbacks
+    simpleBlock();
 }
 
+void (^simpleBlock)(void) = ^{
+    NSLog(@"This is a block");
+};
+
+
+- (void) printArrayToConsole:(NSMutableArray*) myArray
+{
+    NSUInteger size = [myArray count];
+    
+    for (int i = 0; i < size; i++)
+    {
+        NSLog(@"Item %i is: %@", i, [myArray objectAtIndex:i]);
+    }
+    
+    for (id eachObject in myArray)
+    {
+        NSLog(@"Object: %@", eachObject);
+    }
+    
+}
 @end
